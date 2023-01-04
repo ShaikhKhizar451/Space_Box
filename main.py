932041cf1beebb6a685711d3bce72785c55f5031
Shaikh Khizar <khizarsajid@gmail.com>
2023-01-04 18:04:42 +0530

    Adding the link for cors

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = [
+    "http://127.0.0.1:5500",
+    "https://spacebox-j177.onrender.com/",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://spacebox-j177.onrender.com/"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
